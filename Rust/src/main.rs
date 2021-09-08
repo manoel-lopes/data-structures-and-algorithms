e67eddc80a426f12ff9371c72c620e176b8cb464
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-09-08 11:19:04 -0300

    UPDATE RUST

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,3 +1,35 @@
+#![allow(non_snake_case)]
+
+struct Node<T> {
+    next: Option<Box<Node<T>>>,
+    el: T,
+}
+
+struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
+    tail: Option<Box<Node<T>>>,
+    len: i64,
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> LinkedList<T> {
+        LinkedList {
+            head: None,
+            tail: None,
+            len: 0
+        }
+    }
+
+    // fn push(self, el: T) -> i64{
+
+    //     match self{
+    //         Some (self.head) => self.len,
+    //         None => 0
+    //     }
+    // }
+}
+
 fn main() {
+    let list = LinkedList::<i32>::new();
     println!("Hello, world!");
 }
---
+++ Rust/target/rls/debug/deps/Rust-0d7798ddf0152552.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/manoel/Documents/GitHub/Implementations/data-structures-and-algorithms/Rust/target/rls/debug/deps/Rust-0d7798ddf0152552.rmeta: src/main.rs
+
+/mnt/c/Users/manoel/Documents/GitHub/Implementations/data-structures-and-algorithms/Rust/target/rls/debug/deps/Rust-0d7798ddf0152552.d: src/main.rs
+
+src/main.rs:
