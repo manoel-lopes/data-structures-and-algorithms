7e7e2a77e3d83f90f2de9c86ea3e8b95c119365f
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-09-08 21:37:19 -0300

    UPDATE RUST

--- Rust/src/lists/mod.rs
+++ Rust/src/lists/mod.rs
@@ -1,29 +1,40 @@
 struct Node<T> {
-    _next: Option<Box<Node<T>>>,
-    _el: T,
+    next: Option<Box<Node<T>>>,
+    el: T,
+}
+
+impl<T> Node<T> {
+    pub fn new(el: T) -> Self {
+        Node { next: None, el: el }
+    }
 }
 
 pub struct LinkedList<T> {
-    _head: Option<Box<Node<T>>>,
-    _tail: Option<Box<Node<T>>>,
+    head: Option<Box<Node<T>>>,
+    tail: Option<Box<Node<T>>>,
     len: i64,
 }
+
 
 impl<T> LinkedList<T> {
     pub fn new() -> Self {
         LinkedList {
-            _head: None,
-            _tail: None,
+            head: None,
+            tail: None,
             len: 0,
         }
     }
 
-    pub fn push(mut self, _el: T) -> Self {
+
+    pub fn push(mut self, el: T) -> Self {
+        let mut node = Node::<T>::new(el);
+
         self.len += 1;
         self
+        
     }
 
-    pub fn len(self) -> i64 { 
+    pub fn len(self) -> i64 {
         self.len
     }
 }
--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -7,7 +7,7 @@ use lists::LinkedList;
 fn main() {
     let mut list = LinkedList::<i32>::new();
     list = list.push(1);
-    list = list.push(2);
-    list = list.push(3);
+    // list = list.push(2);
+    // list = list.push(3);
     println!("{}", list.len())
 }
