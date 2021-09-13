2ca433b750de3ae2cef48902921b76a4dce24d65
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-09-13 00:40:33 -0300

    UPDATE C

--- C/algorithms/sorting/bubble-sort/regular/main.c
+++ C/algorithms/sorting/bubble-sort/regular/main.c
@@ -50,8 +50,6 @@ void swap(int *array, int i, int j)
 
 void bubbleSort(int *array, int n)
 {
-    int temp;
-
     for (int i = 0; i < n - 1; i++)
         for (int j = n - 1; j > i; j--)
             if (array[j] < array[j - 1])
