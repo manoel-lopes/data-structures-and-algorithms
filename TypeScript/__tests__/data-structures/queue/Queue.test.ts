3c815badd367d9e76557dae487a9c1e7195c7188
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-09-12 21:01:28 -0300

    UPDATE TYPESCRIPT

--- TypeScript/__tests__/data-structures/queue/Queue.test.ts
+++ TypeScript/__tests__/data-structures/queue/Queue.test.ts
@@ -1,4 +1,4 @@
-import Queue from "../../../src/data-structures/queue/entities/Queue"
+import { Queue } from "../../../src/data-structures/queue/entities/Queue"
 
 test('Must insert a element at the end of the linked list', () => {
     const queue = new Queue()
